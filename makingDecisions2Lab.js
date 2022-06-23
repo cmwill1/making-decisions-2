735be91f13337cd19ba5bd1885f9acbee2a18a96
Christina Will <cmkupiec@me.com>
2022-06-23 18:59:59 -0400

    updated question 11

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -207,3 +207,16 @@ switch (letterGrade) {
 */
 
 //Code Here
+// 
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0) {
+    console.log("Dev")
+  } else if (i % 5 === 0) {
+    console.log("mountain")
+  } else if (i % 5 === 0 && i % 3 ===0) {
+    console.log("Devmountain")
+  } else {
+    console.log(i)
+  }
+}
