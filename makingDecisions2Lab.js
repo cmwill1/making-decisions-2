b4258f716e62206026d7f338cb36d94931c948e6
Christina Will <cmkupiec@me.com>
2022-06-23 21:30:26 -0400

    updated error in problem 11

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -210,13 +210,14 @@ switch (letterGrade) {
 // 
 
 for (let i = 1; i <= 100; i++) {
-  if (i % 3 === 0) {
-    console.log("Dev")
-  } else if (i % 5 === 0) {
-    console.log("mountain")
-  } else if (i % 5 === 0 && i % 3 ===0) {
-    console.log("Devmountain")
+  if(i % 3===0 && i % 5===0){
+  console.log('devMountain') 
+  }else if (i % 5 === 0) {
+    console.log('mountain')
+  }else if (i % 3 ===0) {
+    console.log('dev')
   } else {
-    console.log(i)
+    console.log(i);
   }
 }
+
